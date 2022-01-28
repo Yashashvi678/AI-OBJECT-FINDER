5318a4ab4a41bd7a97de3b6663991c3bb628b07e
Yashashvi678 <81162244+Yashashvi678@users.noreply.github.com>
2022-01-28 13:26:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+video = "";
+status = "";
+object_name = "";
+
 function setup()
 {
     canvas = createCanvas(360, 360);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function Start()
+{
+    ObjectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects";
+    object_name = document.getElementById("input").value;
+}
+
+function modelLoaded()
+{
+    console.log("ModelLoaded");
+    status = true;
+}
+
+function draw()
+{
+    image(video, 0, 0, 400, 400);
 }
