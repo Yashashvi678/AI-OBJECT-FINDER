515ad376f229ad14369f67fb5aa754189eb93925
Yashashvi678 <81162244+Yashashvi678@users.noreply.github.com>
2022-02-04 16:22:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = [];
 object_name = "";
 
 function setup()
@@ -13,7 +14,7 @@ function setup()
 function Start()
 {
     ObjectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status = Detecting Objects";
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
     object_name = document.getElementById("input").value;
 }
 
@@ -26,4 +27,48 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 400, 400);
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i = 0 ; i = objects[i].length ; i++)
+        {
+            document.getElementById("status").innerHTML = "Status = Objects Detected";
+            document.getElementById("number_if_objects").innerHTML = "Number Of Objects Detected Are : " + objects[i].length;
+
+            fill("green");
+            percent = floor(objects[i].confidence * 100);
+            noFill();
+            stroke("green");
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            rect(objects[i].x, objects[i].y , objects[i].width, objects[i].height);
+            
+            if(objects[i].label == object_name)
+           {
+             video.stop();
+             objectDetector.detect(gotResult);
+             document.getElementById("number_of_objects").innerHTML = object_name + " Found";
+             synth = window.speechSynthesis;
+             utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+             synth.speak(utterThis);
+           }
+           else
+           {
+             document.getElementById("number_of_objects").innerHTML = object_name + " Not Found";
+           }     
+       }
+            
+        }
+
+    }
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error();
+    }else{
+        console.log(results);
+        objects = results;
+    }
 }
